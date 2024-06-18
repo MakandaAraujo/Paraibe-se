fc1b2f2a0ca58cd548ae47a799719d80335b53e1
MakandaAraujo <98857210+MakandaAraujo@users.noreply.github.com>
2024-06-17 23:00:54 -0300

    Add files via upload

---
+++ contadorDias.js
@@ -0,0 +1,29 @@
+function changeImageROT1() {
+    var image = document.getElementById("image");
+    if (image.src == "resources/logobranco.png") {
+       image.src = "resourses/testeRoteiro/Rot1.jpg";
+    }
+    else {
+       image.src = "resources/testeRoteiro/Rot1.jpg";
+    }
+}
+
+function changeImageROT4() {
+    var image = document.getElementById("image");
+    if (image.src == "resources/logobranco.png") {
+       image.src = "resources/testeRoteiro/Rot4.jpg";
+    }
+    else {
+       image.src = "resources/testeRoteiro/Rot4.jpg";
+    }
+}
+
+function changeImageROT7() {
+    var image = document.getElementById("image");
+    if (image.src == "resources/logobranco.png") {
+       image.src = "resources/testeRoteiro/Rot7.jpg";
+    }
+    else {
+       image.src = "resources/testeRoteiro/Rot7.jpg";
+    }
+}
